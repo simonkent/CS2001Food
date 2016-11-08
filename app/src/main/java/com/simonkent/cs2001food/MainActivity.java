b44927b379e715cb53fadd11af03e65e2f93ffd3
simonkent <simon.kent@simonkent.com>
2016-11-08 12:27:45 +0000

    Added basic interaction when Button is pressed

--- app/src/main/java/com/simonkent/cs2001food/MainActivity.java
+++ app/src/main/java/com/simonkent/cs2001food/MainActivity.java
@@ -2,6 +2,9 @@ package com.simonkent.cs2001food;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.press_button);
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          TextView foodView = (TextView)findViewById(R.id.food_textview);
+                                          foodView.setText("chocolate");
+                                      }
+                                  }
+        );
+
     }
 }
